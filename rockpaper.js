da57bb0ff58c1c323df90de09b7064d83a0ff71b
Dilip2406 <dilipsinghm.mca22@rvce.edu.in>
2023-03-23 22:14:34 +0530

    Update ui and modified .js functions

--- rockpaper.js
+++ rockpaper.js
@@ -1,3 +1,45 @@
+let playerscore=0;
+let compscore=0;
+
+const button=document.querySelectorAll('button');
+button.forEach(button => button.addEventListener('click',respond))
+
+function respond(e)
+{
+    const playerchoice=this.classList.value;
+    const playerSelection= parseInt(this.id);
+    let string=playRound(getComputerChoice(),playerSelection,playerchoice);
+    
+    const div1=document.querySelector(".output");
+    const div=document.querySelector(".game");
+    div.textContent="";
+    div1.textContent=string;
+    const span1=document.querySelector(".compscore");
+    const span2=document.querySelector(".playerscore");
+    span1.textContent=`Computer Score :- ${compscore} `;
+    span2.textContent=`Player Score:- ${playerscore}`;
+    if(playerscore===5)
+    {
+        div.innerHTML=`Player Wins!!<br><br> Refresh the page to play again.`;
+        playerscore=0;
+        compscore=0;
+        button.forEach(button => button.removeEventListener('click',respond));
+
+    }
+    else if (compscore===5)
+    {
+        div.innerHTML=`Computer Wins!!<br><br> Refresh the page to play again.`;
+        playerscore=0;
+        compscore=0;
+        button.forEach(button => button.removeEventListener('click',respond));
+
+    }
+   console.log(playerscore);
+   console.log(compscore);    
+}
+
+
+
 
 function getComputerChoice()
 {
@@ -6,17 +48,14 @@ function getComputerChoice()
     
 }
 
-function getPlayerChoice()
-{
-    let num=Math.ceil(Math.random()*3);
-    
-    return num;
-}
 
 
-function playRound(playerSelection, computerSelection) {
+
+
+
+function playRound(computerSelection,playerSelection,playerchoice) {
     let string;
-    let playerchoice;
+  
     let compchoice;
     if(computerSelection===1)
     {
@@ -31,18 +70,6 @@ function playRound(playerSelection, computerSelection) {
         compchoice="Scissors";
     };
     
-    if(playerSelection===1)
-    {
-        playerchoice="Rock";
-    }    
-    else if(playerSelection===2)
-    {
-        playerchoice="Paper";
-    }
-    else if(playerSelection===3)
-    {
-        playerchoice="Scissors";
-    };
 
     if(playerSelection===computerSelection)
     {
@@ -50,55 +77,31 @@ function playRound(playerSelection, computerSelection) {
         return string;
     }
     else if((playerSelection-computerSelection)===-1){
-        string=`You lose ${compchoice} beats ${playerchoice}!`;
+        string=`You lose, ${compchoice} beats ${playerchoice}!`;
+        compscore++
         return string;
     }
     else if((playerSelection-computerSelection)===1){
-        string=`You win ${playerchoice} beats ${compchoice}!`;
+        string=`You win, ${playerchoice} beats ${compchoice}!`;
+        playerscore++
         return string;
     }
     else if((playerSelection-computerSelection)===-2){
-        string=`You win ${playerchoice} beats ${compchoice}!`;
+        string=`You win, ${playerchoice} beats ${compchoice}!`;
+        playerscore++;
         return string;
     }
     else if((playerSelection-computerSelection)===2){
-        string=`You lose ${compchoice} beats ${playerchoice}!`;
+        string=`You lose, ${compchoice} beats ${playerchoice}!`;
+        compscore++;
         return string;
     }
+
+
+    
   }
 
 
-function game()
-{
-    let player=0;
-    let computer=0;
-    let NumberofRounds=prompt("Enter number of rounds:");
-    for(let i=0;i<NumberofRounds;i++)
-    {
-        string=playRound(getComputerChoice(),getPlayerChoice());
-        console.log(string);
-        if(string[4]==='W')
-        {
-            player++;
-        }
-        else if(string[4]=='L')
-        {
-            computer++;
-        }
-    };
-    if (player===computer)
-    {
-        console.log("Game:It's a tie!");
-    }
-    else if (computer>player)
-    {
-        console.log("Computer Wins!");
-    }
-    else if(player>computer)
-    {
-        console.log("Player wins!");
-    };
-}
 
 
-game();
+    
